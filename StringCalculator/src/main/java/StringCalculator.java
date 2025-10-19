b294ee7ca0a3f626dae0a2b7cff8b0180fe223fb
GopalMoolchandani <gopalmcpmulchandani@gmail.com>
2025-10-19 17:47:11 +0530

    Add method to allow any amount of numbers

--- StringCalculator/src/main/java/StringCalculator.java
+++ StringCalculator/src/main/java/StringCalculator.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class StringCalculator {
+    public static int add(String numbers) {
+        int result = 0;
+        String[] numbersArray = numbers.split(",");
+        for (String i : numbersArray) {
+            int num = 0;
+            try {
+                num = Integer.parseInt(i);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Invalid number: " + i);
+            }
+            result+=num;
+        }
+        return result;
+    }
 }
--- StringCalculator/src/test/java/StringCalculatorTest.java
+++ StringCalculator/src/test/java/StringCalculatorTest.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
+    @Test
+    public void emptyStringReturnsZero() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
+    @Test
+    public void singleNumberReturnsValue() {
+        assertEquals(1, StringCalculator.add("1"));
+    }
+
+    @Test
+    public void twoNumbersCommaSeparated() {
+        assertEquals(6, StringCalculator.add("1,5"));
+    }
+
+    @Test
+    public void multipleNumbers() {
+        assertEquals(15,StringCalculator.add("1,2,3,4,5"));
+    }
+
+    @Test
+    public void allowAnyAmountOfNumbers() {
+        // large list
+        StringBuilder sb = new StringBuilder();
+        int expected = 0;
+        for (int i=1;i<=20;i++){
+            if (i>1) sb.append(",");
+            sb.append(i);
+            expected += i;
+        }
+        assertEquals(expected, StringCalculator.add(sb.toString()));
+    }
 }
